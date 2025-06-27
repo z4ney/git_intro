c3625cc56a01df8ed02efd56dedbb6030e1eefc0
z4ney <bchan10@g.ucla.edu>
2025-06-26 19:00:53 -0700

    new mean function

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,10 @@
 print("hey world!!!")
 print("bye bye!!!")
 print("no no no")
+
+# new mean function
+def mean(input):
+  sum = 0
+  for entry in input:
+    sum += entry
+  return sum / len(input)

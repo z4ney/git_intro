d53b985a0228d80767e3a8521891c8bc082bc094
z4ney <bchan10@g.ucla.edu>
2025-06-26 19:03:50 -0700

    added median function

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,16 @@
 print("hey world!!!")
 print("bye bye!!!")
 print("no no no")
+
+# new median function
+def median(inputList):
+  median = -1
+  listLength = len(inputList)
+  middleIndex = (listLength - 1) // 2
+
+  if listLength // 2 == 1:
+    median = inputList(middleIndex)
+  else:
+    median = (inputList[middleIndex] + inputList[middleIndex + 1]) / 2
+
+  return median
